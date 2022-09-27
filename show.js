9b264d2b00620703104d676e89954efd63d375fc
juan49278 <juan49278@gmail.com>
2022-09-27 03:48:07 -0300

    commit

--- show.js
+++ show.js
@@ -11,20 +11,26 @@ function show(){
         toAppened += `<tr>
         <th scope="row">${[i+1]}</th>
         <td class="col-span-2">${data.productos.sort()[i]}</td>
-        <td class="col-span-2"><input type="text" class="cant">
-        <td class="col-span-2"><input type="text" class="precio">
-        <td class="col-span-2"><span id="${length}total">Total</span>
+        <td class="col-span-2"><input type="number" class="cant" value="">
+        <td class="col-span-2"><input type="number" class="precio" value="">
+        <td class="col-span-2"><span id="${i}total"></span>
         </tr>`
     }
     document.getElementById('productos').innerHTML = toAppened
 }
 function calcular(){
 let todosInputs = document.querySelectorAll('input')
-for(productos of todosInputs){
-    i = []
-let a = document.querySelectorAll('input')[i].value
-let b = document.querySelectorAll('input')[i+1].value
+for(let i=0; i < todosInputs.length ;i ++){
+let a = document.querySelectorAll('input.cant')[i].value
+let b = document.querySelectorAll('input.precio')[i].value
 let c = b * a
-document.querySelectorAll('span')[i].innerHTML = c
+document.querySelectorAll('span')[i].innerHTML = '$' + ' ' + c
 }
 }
+
+/* document.getElementById('saveProgress').addEventListener('click', ()=> {
+    let table = document.getElementById('table')
+    if(!localStorage.getItem('table')){
+        localStorage.setItem('table', table.innerHTML)
+    }
+}) */
