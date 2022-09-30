38245d0ff394d8c44bd0ed933ff757f4e541245f
juan49278 <juan49278@gmail.com>
2022-09-30 02:29:26 -0300

    commit

--- show.js
+++ show.js
@@ -13,7 +13,7 @@ function show(){
         <td class="col-span-2">${data.productos.sort()[i]}</td>
         <td class="col-span-2"><input type="number" class="cant" value="">
         <td class="col-span-2"><input type="number" class="precio" value="">
-        <td class="col-span-2"><span id="${i}total"></span>
+        <td class="col-span-2"><span class="total" id="${i}total"></span>
         </tr>`
     }
     document.getElementById('productos').innerHTML = toAppened
@@ -24,10 +24,19 @@ for(let i=0; i < todosInputs.length ;i ++){
 let a = document.querySelectorAll('input.cant')[i].value
 let b = document.querySelectorAll('input.precio')[i].value
 let c = b * a
-document.querySelectorAll('span')[i].innerHTML = '$' + ' ' + c
+document.querySelectorAll('span')[i].innerHTML = c
 }
 }
 
+function sumatoria(){
+    var resultTotal = document.querySelectorAll('span')
+   for(let i=0; i < resultTotal.length; i++){
+    let valor = document.querySelectorAll('span.total')[i];
+    let valorX = document.querySelectorAll('span.total')[i+1];
+   let result = valor + valorX;
+   document.getElementById('result').innerHTML = result
+    }
+}
 /* document.getElementById('saveProgress').addEventListener('click', ()=> {
     let table = document.getElementById('table')
     if(!localStorage.getItem('table')){
