fb4de0026260ca2308f3e4b8a14e1c25067c7463
juan49278 <110367048+juan49278@users.noreply.github.com>
2022-10-14 05:00:29 -0300

    Update showCongelados.js

--- showCongelados.js
+++ showCongelados.js
@@ -14,8 +14,8 @@ addEventListener('DOMContentLoaded', async()=>{
             <th scope="row">${[i+1]}</th>
             <td class="col-span-2">${data.productos.sort()[i]}</td>
             <td class="col-span-2"><input type="number" class="form-control cant col-8 col-md-3" value="" aria-label="cant"><p class="cant d-none"></p>
-            <td class="col-span-2"><input type="number" class="form-control precio col-10 col-md-3" value="" aria-label="precio"><p class="precio d-none"></p>
-            <td onclick="calcular()" class="col-span-2"><span class="total" id="${i}total"></span>
+            <td class="col-span-2"><input type="number" oninput="calcular()" class="form-control precio col-10 col-md-3" value="" aria-label="precio"><p class="precio d-none"></p>
+            <td class="col-span-2"><span class="total" id="${i}total"></span>
             </tr>`
         }
         document.getElementById('productos').innerHTML = toAppened
