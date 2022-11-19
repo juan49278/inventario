81cecfcc5c7bd386748ece5c7a60ae87d3213c7e
juan49278 <juan49278@gmail.com>
2022-11-19 19:47:16 -0300

    commit

--- js/login.js
+++ js/login.js
@@ -1,12 +1,32 @@
-(() => {
-    const forms = document.querySelectorAll('.needs-validation')
-    Array.from(forms).forEach(form => {
-      form.addEventListener('submit', event => {
-        if (!form.checkValidity()) {
-          event.preventDefault()
-          event.stopPropagation()
-        }
-        form.classList.add('was-validated')
-      }, false)
-    })
-  })()
+function checkedForm(){
+  if(!checkpass()){
+    password.classList.add('is-invalid')
+  } else{
+    password.classList.remove('is-invalid')
+  } if(!checkuser()){
+    user.classList.add("is-invalid")
+  } else {
+    user.classList.remove('is-invalid')
+  }
+  if(checkpass() && checkuser()){
+    window.location.replace("index.html")
+  }
+}
+function checkuser(){
+  if(user.value === 'admin'){
+    user.setCustomValidity("")
+    return true
+  } else{
+    user.setCustomValidity("error")
+    return false
+  }
+}
+function checkpass(){
+  if(password.value === "frigo2004"){
+    password.setCustomValidity("")
+    return true
+  } else{
+    password.setCustomValidity("error")
+    return false
+  }
+}
